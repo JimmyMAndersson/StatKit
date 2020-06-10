08362b2166c3fe90e78513f94c64c3188c5ac0a7
Jimmy M Andersson <dev@applyn.se>
2020-06-10 22:47:22 +0200

    Make it possible to pick the random variable to calculate over

--- Sources/StatKit/Descriptive Statistics/Averages.swift
+++ Sources/StatKit/Descriptive Statistics/Averages.swift
@@ -1,48 +1,63 @@
-extension Collection where Element: ConvertibleToDouble {
-  /**
-   Calculates the arithmetic mean of all contained elements.
-   - returns: The mean of all items.
-   Because the arithmetic mean has no meaning on an empty set, this method returns a NaN if the collection is empty.
-   */
+extension Collection {
+  
+  /// Calculates the arithmetic mean of the selected variable.
+  /// - parameter variable: The variable over which to calculate the mean.
+  /// - returns: The mean of all items.
+  /// Since the arithmetic mean has no meaning on an empty set, this method returns a NaN if the collection is empty.
+  /// The time complexity of this method is O(n).
   @inlinable
-  public var arithmeticMean: Double {
-    !self.isEmpty ? sum.doubleValue / Double(count) : .nan
+  public func arithmeticMean<T: ConvertibleToDouble>(over variable: KeyPath<Element, T>) -> Double {
+    !isEmpty ? sum(over: variable).doubleValue / Double(count) : .nan
   }
-}
-
-extension Collection where Element: Comparable & ConvertibleToDouble {
+  
+  /// Calculates the median of the selected variable.
+  /// - parameter variable: The variable over which to calculate the median.
+  /// - returns: The median of all items.
+  /// Since the median has no meaning on an empty set, this method returns a NaN if the collection is empty.
+  /// The time complexity of this method is O(n).
   @inlinable
-  public func median() -> Double {
+  public func median<T: ConvertibleToDouble & Comparable>(over variable: KeyPath<Element, T>) -> Double {
     guard count > 0 else { return .nan }
     
     let isEvenNumberOfElements = count % 2 == 0
-    let sortedElements = self.sorted()
+    let sortedElements = self.sorted(by: { $0[keyPath: variable] < $1[keyPath: variable] })
     
     let index = (count - 1) / 2
-    let median = isEvenNumberOfElements
-      ? (sortedElements[index].doubleValue + sortedElements[index + 1].doubleValue) / 2
-      : sortedElements[index].doubleValue
+    
+    var median: Double
+    
+    if isEvenNumberOfElements {
+      let firstValue = sortedElements[index][keyPath: variable].doubleValue
+      let secondValue = sortedElements[index + 1][keyPath: variable].doubleValue
+      median = (firstValue + secondValue) / 2
+    } else {
+      median = sortedElements[index][keyPath: variable].doubleValue
+    }
     
     return median
   }
 }
 
-extension Sequence where Element: Hashable {
-  /**
-   Finds the mode(s) of the sequence.
-   - returns: A set containing the mode(s) sorted in ascending order.
-   The mode of a sequence is the item that occurs most frequently.
-   A sequence that has a single item that is most occuring is unimodal,
-   as opposed to sequences that have several items that occur equally often and are called multimodal.
-   The time complexity of `mode()` is O(n).
-   */
+extension Sequence {
+  
+  /// Finds the mode(s) of the sequence.
+  /// - parameter variable: The variable over which to calculate the mode(s).
+  /// - returns: A set containing the mode(s) sorted in ascending order.
+  /// The mode of a sequence is the item that occurs most frequently.
+  /// A sequence that has a single item that is most occuring is unimodal,
+  /// as opposed to sequences that have several items that occur equally often and are called multimodal.
+  /// The time complexity of this method is O(n).
   @inlinable
-  public func mode() -> Set<Element> {
-    let dictionary = self.reduce(into: [Element: Int]()) { (result, element) in result[element, default: 0] += 1 }
+  public func mode<T: Hashable>(over variable: KeyPath<Element, T>) -> Set<T> {
+    let dictionary = self.reduce(into: [T: Int]()) { (result, element) in
+      result[element[keyPath: variable], default: 0] += 1
+    }
+    
     let maximumOccurence = dictionary.values.max() ?? 0
+    
     let result = dictionary
-      .filter { elementCount in elementCount.value == maximumOccurence }
-      .map { elementCount in elementCount.key }
+      .filter { variableCount in variableCount.value == maximumOccurence }
+      .map { variableCount in variableCount.key }
     
     return Set(result)
   }
