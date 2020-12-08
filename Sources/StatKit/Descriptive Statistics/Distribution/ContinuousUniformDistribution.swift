a58346fdd72ca9d0fe1ab53e1fed401fec847ba6
Jimmy M Andersson <dev@applyn.se>
2020-12-08 21:37:11 +0100

    Add Continuous Uniform Distribution

---
+++ Sources/StatKit/Descriptive Statistics/Distribution/ContinuousUniformDistribution.swift
@@ -0,0 +1,57 @@
+public typealias UniformDistribution = ContinuousUniformDistribution
+
+public struct ContinuousUniformDistribution: ContinuousDistribution {
+  public let lowerBound: Double
+  public let upperBound: Double
+  
+  public init(_ lowerBound: Double, _ upperBound: Double) {
+    guard lowerBound < upperBound else {
+      fatalError("The lower bound needs to be strictly less than the upper bound.")
+    }
+    self.lowerBound = lowerBound
+    self.upperBound = upperBound
+  }
+  
+  public func pdf(x: Double) -> Double {
+    guard lowerBound <= x && x <= upperBound else { return 0 }
+    return 1 / (upperBound - lowerBound)
+  }
+  
+  public var mean: Double {
+    return (lowerBound + upperBound) / 2
+  }
+  
+  public var variance: Double {
+    let difference = upperBound - lowerBound
+    let differenceSquared = difference * difference
+    return differenceSquared / 12
+  }
+  
+  public var skewness: Double {
+    return 0
+  }
+  
+  public var kurtosis: Double {
+    return 9 / 5
+  }
+  
+  public func cdf(x: Double) -> Double {
+    switch x {
+      case ..<lowerBound:
+        return 0
+      case lowerBound...upperBound:
+        return (x - lowerBound) / (upperBound - lowerBound)
+      default:
+        return 1
+    }
+  }
+  
+  public func sample() -> Double {
+    return .random(in: lowerBound ... upperBound)
+  }
+  
+  public func sample(_ numberOfElements: Int) -> [Double] {
+    return (0 ..< numberOfElements)
+      .map { _ in Double.random(in: lowerBound ... upperBound) }
+  }
+}
--- Sources/StatKit/Descriptive Statistics/Distribution/Distribution.swift
+++ Sources/StatKit/Descriptive Statistics/Distribution/Distribution.swift
@@ -1,22 +1,27 @@
+/// A protocol specifying requirements for probability distributions.
 public protocol Distribution {
-  associatedtype Element
+  /// The type of value that the distribution deals with (for example, reals or integers).
+  associatedtype Value
   
+  /// The distributions expected value.
   var mean: Double { get }
+  /// The distributions variance.
   var variance: Double { get }
+  /// The distributions skewness.
   var skewness: Double { get }
+  /// The kurtosis of the distribution.
   var kurtosis: Double { get }
   
-  func CDF(x: Element) -> Double
+  /// Calculates the cumulative density for a value in the distribution.
+  /// - parameter x: The value used to compute the cumulative density.
+  /// - returns: A number in [0, 1] specifying the proportion of values that are less than or equal to x.
+  func cdf(x: Value) -> Double
   
-  func percentPointFunction(x: Element) -> Double
+  /// Samples a single value from the distribution.
+  /// - returns: A sample from the distribution.
+  func sample() -> Value
   
-  func survivialFunction(x: Element) -> Double
-  func inverseSurvivalFunction(x: Element) -> Double
-  
-  func moment(_ type: MomentType, order: Int) -> Double
-}
-
-public enum MomentType {
-  case central
-  case raw
+  /// Samples a specified number of values from the distribution.
+  /// - returns: An array of sampled values.
+  func sample(_ numberOfElements: Int) -> [Value]
 }
