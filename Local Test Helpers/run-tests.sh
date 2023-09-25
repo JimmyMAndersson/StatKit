6f1aa7f1cb638a59c181accf95253f0d883eeedf
Jimmy M Andersson <dev@applyn.se>
2023-09-25 21:01:08 +0200

    tests: Move helpers for local tests into separate directory

--- Local Test Helpers/run-tests.sh
+++ Local Test Helpers/run-tests.sh
@@ -4,4 +4,4 @@ else
     echo "Install Docker to run Linux tests and linting..."
 fi
 
-xcodebuild -scheme StatKit -destination 'platform=macOS' -configuration release -quiet build test
+swift test -c release --parallel
